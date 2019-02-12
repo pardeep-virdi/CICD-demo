c4fdd67dcebbc65504e6b0af696f9c553fc59f0a
pardeep-virdi <31764809+pardeep-virdi@users.noreply.github.com>
2019-02-12 14:35:20 +0530

    Create mavenbuild.groovy

---
+++ groovy-code/mavenbuild.groovy
@@ -0,0 +1,25 @@
+node {
+
+stage('Clone sources') {
+        git branch: 'pipeline', url: 'https://github.com/pardeep-virdi/config-server.git'
+    }
+    
+    
+stage('Maven build') {
+        sh 'mvn clean  install' 
+    }
+
+stage('build docker image') {
+    
+    docker.withRegistry('', 'mydockerid') {
+    
+    def dockerfile = 'dockerfile'
+    def customImage = docker.build("pardeepvirdi/config-server:${env.BUILD_ID}", "-f src/main/docker/${dockerfile} . ") 
+    
+    customImage.push()
+     
+    }
+    
+}
+    
+   }
