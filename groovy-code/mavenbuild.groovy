2104445a9d104374da03469517031c0be548a4e3
pardeep-virdi <31764809+pardeep-virdi@users.noreply.github.com>
2019-02-13 12:21:28 +0530

    Update mavenbuild.groovy

--- groovy-code/mavenbuild.groovy
+++ groovy-code/mavenbuild.groovy
@@ -2,7 +2,7 @@ node {
 
 stage('Clone sources') {
        // git branch: 'pipeline', url: 'https://github.com/pardeep-virdi/config-server.git'
-     checkout([$class: 'GitSCM', branches: [[name: '*/pipeline']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/pardeep-virdi/config-server.git']]])
+     checkout([$class: 'GitSCM', branches: [[name: '*/pipeline']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/pardeep-virdi/maven.git']]])
     }
     
     
@@ -15,7 +15,7 @@ stage('build docker image') {
     docker.withRegistry('', 'mydockerid') {
     
     def dockerfile = 'dockerfile'
-    def customImage = docker.build("pardeepvirdi/config-server:${env.BUILD_ID}", "-f src/main/docker/${dockerfile} . ") 
+    def customImage = docker.build("pardeepvirdi/maven-project:${env.BUILD_ID}", "-f src/main/docker/${dockerfile} . ") 
     
     customImage.push()
      
